38682253bc4391d6ca6e2f047e0eeb99f3900900
Patrik Husfloen <redoz@redoz.com>
2026-07-18 18:19:20 +0200

    feat(web): convert node leaf rows to Atlas classes
    Plan: atlas-restyle
    Plan-Tasks: Task 2

--- packages/web/src/components/canvas/nodes/StereotypeRow.svelte
+++ packages/web/src/components/canvas/nodes/StereotypeRow.svelte
@@ -3,8 +3,8 @@
 </script>
 
 {#if keyword || stereotypes.length > 0}
-  <div class="px-3 pt-[7px] text-center text-[10.5px] leading-tight text-slate-500">
-    {#if keyword}<span class="block">{`«${keyword}»`}</span>{/if}
-    {#each stereotypes as s (s)}<span class="mr-1">{`«${s}»`}</span>{/each}
+  <div class="node-stereo">
+    {#if keyword}<span>{`«${keyword}»`}</span>{/if}
+    {#each stereotypes as s (s)}<span>{`«${s}»`}</span>{/each}
   </div>
 {/if}
